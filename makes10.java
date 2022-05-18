93b2c4ca7cbd3df513ed6c1093b9ad5929be3c09
Kaigh Taylor <coders@kai-deas.com>
2022-05-18 16:34:59 -0400

    CodeBat | makes10 code

---
+++ makes10.java
@@ -0,0 +1,10 @@
+public class Main {
+    public boolean makes10(int a, int b) {
+        boolean makers;
+        if ((a == 10) || (b == 10) || (a+b == 10)) {
+        makers = true;
+        }
+        else makers = false;
+        return makers;
+    }
+}  
